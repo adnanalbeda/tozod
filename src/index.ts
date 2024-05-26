239b391ddaa305e81729e8dfac2e340aff6efa5f
Adnan AlBeda <adnan.albeda@outlook.com>
2024-05-26 18:41:06 +0300

    Fixed toZod for `default` and `refine` rules.
    N/A
    
    Since the library is abandoned, go copy the code and paste it in your project.

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,43 @@
-import * as z from 'zod';
+import { z } from "zod";
 
-type isAny<T> = [any extends T ? 'true' : 'false'] extends ['true'] ? true : false;
-type nonoptional<T> = T extends undefined ? never : T;
-type nonnullable<T> = T extends null ? never : T;
-type equals<X, Y> = [X] extends [Y] ? ([Y] extends [X] ? true : false) : false;
+type IsAny<T> =
+  [any extends T ? "true" : "false"] extends ["true"] ? true : false;
+type NonOptional<T> = T extends undefined ? never : T;
+type equals<X, Y> =
+  [X] extends [Y] ?
+    [Y] extends [X] ?
+      true
+    : false
+  : false;
+
+type toZodExtended<T extends z.ZodTypeAny> =
+  | T
+  | z.ZodDefault<T>
+  | z.ZodUnion<[T, ...z.ZodTypeAny[]]>
+  | z.ZodEffects<T>;
 
 export type toZod<T> = {
-  any: never;
-  optional: z.ZodUnion<[toZod<nonoptional<T>>, z.ZodUndefined]>;
-  nullable: z.ZodUnion<[toZod<nonnullable<T>>, z.ZodNull]>;
-  array: T extends Array<infer U> ? z.ZodArray<toZod<U>> : never;
-  string: z.ZodString;
-  bigint: z.ZodBigInt;
-  number: z.ZodNumber;
-  boolean: z.ZodBoolean;
-  date: z.ZodDate;
-  object: z.ZodObject<{ [k in keyof T]: toZod<T[k]> }, { strict: true }, T>;
-  rest: never;
+  any: z.ZodTypeAny;
+  optional: toZodExtended<z.ZodOptional<toZod<NonOptional<T>>>>;
+  nullable: toZodExtended<z.ZodNullable<toZod<NonNullable<T>>>>;
+  array: T extends Array<infer U> ? toZodExtended<z.ZodArray<toZod<U>>> : never;
+  string: toZodExtended<z.ZodString>;
+  bigint: toZodExtended<z.ZodBigInt>;
+  number: toZodExtended<z.ZodNumber>;
+  boolean: toZodExtended<z.ZodBoolean>;
+  date: toZodExtended<z.ZodDate>;
+  object: toZodExtended<z.ZodObject<{ [k in keyof T]: toZod<T[k]> }>>;
 }[zodKey<T>];
 
-type zodKey<T> = isAny<T> extends true
-  ? 'any'
-  : equals<T, boolean> extends true //[T] extends [booleanUtil.Type]
-  ? 'boolean'
-  : [undefined] extends [T]
-  ? 'optional'
-  : [null] extends [T]
-  ? 'nullable'
-  : T extends any[]
-  ? 'array'
-  : equals<T, string> extends true
-  ? 'string'
-  : equals<T, bigint> extends true //[T] extends [bigintUtil.Type]
-  ? 'bigint'
-  : equals<T, number> extends true //[T] extends [numberUtil.Type]
-  ? 'number'
-  : equals<T, Date> extends true //[T] extends [dateUtil.Type]
-  ? 'date'
-  : T extends { [k: string]: any } //[T] extends [structUtil.Type]
-  ? 'object'
-  : 'rest';
+type zodKey<T> =
+  IsAny<T> extends true ? "any"
+  : undefined extends T ? "optional"
+  : null extends T ? "nullable"
+  : equals<T, string> extends true ? "string"
+  : equals<T, bigint> extends true ? "bigint"
+  : equals<T, number> extends true ? "number"
+  : equals<T, boolean> extends true ? "boolean"
+  : equals<T, Date> extends true ? "date"
+  : T extends any[] ? "array"
+  : T extends { [k: string]: any } ? "object"
+  : "any";
